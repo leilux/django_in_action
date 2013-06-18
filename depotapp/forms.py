84f14a52ab30e4405ff9c7d73038d77f1caff18c
leilux <27kilua@gmail.com>
2013-06-18 15:26:05 +0800

    #9 Product Input Validation

--- depotapp/forms.py
+++ depotapp/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from models import *
 
 
+ends = lambda s, suffixs: any(map(lambda x: s.endswith(x), suffixs))
 
 class ProductForm(forms.ModelForm):
 	
@@ -12,4 +13,17 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ProductForm, self).__init__(*args, **kwargs)
+
+    def clean_price(self):
+        price = self.cleaned_data['price']
+        if price <= 0:
+            raise forms.ValidationError('price must > 0')
+        return price
+
+    def clean_image_url(self):
+        url = self.cleaned_data['image_url']
+        suffixs = ['.jpg', '.png', '.gif']
+        if not ends(url, suffixs):
+            raise forms.ValidationError('img must %s' % str(suffixs))
+        return url
 
--- depotapp/models.py
+++ depotapp/models.py
@@ -3,9 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
-    image_url = models.CharField(max_length=200)
+    image_url = models.URLField(max_length=200)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-
-
