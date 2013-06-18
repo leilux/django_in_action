90c9e6a9d2f717f08753c417f0352a59042f23d4
leilux <27kilua@gmail.com>
2013-06-18 13:36:33 +0800

    #5 bootstrap set static resource

---
+++ django_in_action/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_in_action.views.home', name='home'),
+    # url(r'^django_in_action/', include('django_in_action.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^depotapp/', include('depotapp.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+#5
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
