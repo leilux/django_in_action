467190b6d7d863d2b803d83c69dc24c11f702ff3
leilux <27kilua@gmail.com>
2013-06-18 16:25:03 +0800

    #11 modified Model class

--- depotapp/models.py
+++ depotapp/models.py
@@ -7,3 +7,7 @@ class Product(models.Model):
     description = models.TextField()
     image_url = models.URLField(max_length=200)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    # python manage.py dbshell
+    # alter table depotapp_product add column date_available date not null default 0;
+    # commit;
+    data_available = models.DateField()
